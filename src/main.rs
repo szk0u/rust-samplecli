b6e2caae58f3df54f27f44b4301135dca6374de2
szk0u <szk0u3@gmail.com>
2023-01-02 18:26:07 +0900

    関数化

--- src/main.rs
+++ src/main.rs
@@ -22,13 +22,17 @@ fn main() {
         let f = File::open(path).unwrap();
         let reader = BufReader::new(f);
 
-        for line in reader.lines() {
-            let line = line.unwrap();
-            println!("{}", line);
-        }
+        run(reader, cli.verbose);
     } else {
         println!("No file specified");
     }
 
     println!("Is verbosity specified?: {}", cli.verbose);
+}
+
+fn run(reader: BufReader<File>, verbose: bool) {
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+    }
 }
